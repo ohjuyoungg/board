d9366ff1558a2a4db2d50ce4121fed6a8d946c3f
ohjuyoungg <ejrm12@naver.com>
2025-07-18 18:57:21 +0900

    initial commit

---
+++ src/main/java/Board/Juyoung/JuyoungApplication.java
@@ -0,0 +1,12 @@
+package Board.Juyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuyoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuyoungApplication.class, args);
+	}
+}
