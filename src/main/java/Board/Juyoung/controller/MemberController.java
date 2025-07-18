0aca6435a52bb4b5431d0376297d710d8dda8c77
ohjuyoungg <ejrm12@naver.com>
2025-07-18 20:49:04 +0900

    feat: MemberController API 추가

---
+++ src/main/java/Board/Juyoung/controller/MemberController.java
@@ -0,0 +1,23 @@
+package Board.Juyoung.controller;
+
+import Board.Juyoung.controller.dto.request.MemberCreateRequest;
+import Board.Juyoung.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public String register(@RequestBody MemberCreateRequest memberCreateRequest) {
+        memberService.register(memberCreateRequest);
+        return "ok";
+    }
+}
---
+++ src/main/java/Board/Juyoung/controller/dto/request/MemberCreateRequest.java
@@ -0,0 +1,2 @@
+package Board.Juyoung.controller.dto.request;public record MemberCreateRequest() {
+}
