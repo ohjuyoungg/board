0b703509615beed189bffcfe803de6c1f128ebf4
ohjuyoungg <ejrm12@naver.com>
2025-07-18 20:50:23 +0900

    feat: MemberService 설정

--- src/main/java/Board/Juyoung/service/MemberService.java
+++ src/main/java/Board/Juyoung/service/MemberService.java
@@ -1,2 +1,20 @@
-package Board.Juyoung.service;public class MemberService {
+package Board.Juyoung.service;
+
+import Board.Juyoung.controller.dto.request.MemberCreateRequest;
+import Board.Juyoung.entity.Member;
+import Board.Juyoung.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void register(MemberCreateRequest memberCreateRequest) {
+        Member member = new Member(memberCreateRequest.email(), memberCreateRequest.password(),
+            memberCreateRequest.nickname());
+        memberRepository.save(member);
+    }
 }
