bf33c73924944cde8abedf282eb5532574c2cd32
ohjuyoungg <ejrm12@naver.com>
2025-07-19 17:05:25 +0900

    feat: member 단건 조회, 전체 조회 메서드 구현

--- src/main/java/Board/Juyoung/service/MemberService.java
+++ src/main/java/Board/Juyoung/service/MemberService.java
@@ -2,8 +2,10 @@ package Board.Juyoung.service;
 
 import Board.Juyoung.controller.dto.request.MemberCreateRequest;
 import Board.Juyoung.controller.dto.request.MemberUpdateRequest;
+import Board.Juyoung.controller.dto.response.MemberResponse;
 import Board.Juyoung.entity.Member;
 import Board.Juyoung.repository.MemberRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,5 +33,19 @@ public class MemberService {
         member.changeEmail(memberUpdateRequest.email());
         member.changePassword(memberUpdateRequest.password());
         member.changeNickname(memberUpdateRequest.nickname());
+    }
+
+    @Transactional(readOnly = true)
+    public MemberResponse getMember(Long id) {
+        Member member = memberRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("존재하지 않는 유저입니다."));
+        return MemberResponse.of(member);
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberResponse> getMembers() {
+        return memberRepository.findAll().stream()
+            .map(MemberResponse::of) // m -> MemberResponse.of(m)
+            .toList();
     }
 }
