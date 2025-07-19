b50e69fc677c867b965553127087d25159d6c20c
ohjuyoungg <ejrm12@naver.com>
2025-07-19 15:46:18 +0900

    feat: service 클래스 내에 update 메서드 구현

--- src/main/java/Board/Juyoung/service/MemberService.java
+++ src/main/java/Board/Juyoung/service/MemberService.java
@@ -1,10 +1,12 @@
 package Board.Juyoung.service;
 
 import Board.Juyoung.controller.dto.request.MemberCreateRequest;
+import Board.Juyoung.controller.dto.request.MemberUpdateRequest;
 import Board.Juyoung.entity.Member;
 import Board.Juyoung.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +22,14 @@ public class MemberService {
 
     public void delete(Long id) {
         memberRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void update(MemberUpdateRequest memberUpdateRequest, Long id) {
+        Member member = memberRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("존재하지 않는 유저입니다."));
+        member.changeEmail(memberUpdateRequest.email());
+        member.changePassword(memberUpdateRequest.password());
+        member.changeNickname(memberUpdateRequest.nickname());
     }
 }
