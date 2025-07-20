d872b76c0c84497061636651018a3a957915bf49
ohjuyoungg <ejrm12@naver.com>
2025-07-21 01:14:47 +0900

    feat: BoardResponse DTO 구현

--- src/main/java/Board/Juyoung/controller/dto/response/BoardResponse.java
+++ src/main/java/Board/Juyoung/controller/dto/response/BoardResponse.java
@@ -1,2 +1,13 @@
-package Board.Juyoung.controller.dto.response;public record BoardResponse() {
+package Board.Juyoung.controller.dto.response;
+
+import Board.Juyoung.entity.Board;
+import Board.Juyoung.entity.Member;
+
+public record BoardResponse(Long memberId, String nickname, Long boardId, String title, String content, String image) {
+
+    public static BoardResponse of(Board board) {
+        Member member = board.getMember();
+        return new BoardResponse(member.getId(), member.getNickname(), board.getId(), board.getTitle(),
+            board.getContent(), board.getImage());
+    }
 }
