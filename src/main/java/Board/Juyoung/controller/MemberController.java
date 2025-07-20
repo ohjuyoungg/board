5a0496d75ec1f4f3c25470c982eef804ed9f841b
ohjuyoungg <ejrm12@naver.com>
2025-07-21 01:13:41 +0900

    refactor: 애노테이션 순서 변경

--- src/main/java/Board/Juyoung/controller/MemberController.java
+++ src/main/java/Board/Juyoung/controller/MemberController.java
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
+@RestController
 public class MemberController {
 
     private final MemberService memberService;
