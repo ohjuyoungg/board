fa1f388260a95b24b86fcceb780167ea6bfdb18d
ohjuyoungg <ejrm12@naver.com>
2025-07-21 22:04:50 +0900

    feat: 테스트 sql 파일 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+INSERT INTO member (id, email, password, nickname) VALUES (1, 'posil@example.com', 'pass1', '포실이');
+INSERT INTO member (id, email, password, nickname) VALUES (2, 'happy@example.com', 'pass2', '행복이');
+INSERT INTO member (id, email, password, nickname) VALUES (3, 'JJingJJing@example.com', 'pass3', '찡찡이');
+INSERT INTO member (id, email, password, nickname) VALUES (4, 'makdong@example.com', 'pass4', '막동이');
+
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 1', 'Content 1', 'img1.jpg', 1);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 2', 'Content 2', 'img2.jpg', 1);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 3', 'Content 3', 'img3.jpg', 1);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 4', 'Content 4', 'img4.jpg', 1);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 5', 'Content 5', 'img5.jpg', 1);
+
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 6', 'Content 6', 'img6.jpg', 2);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 7', 'Content 7', 'img7.jpg', 2);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 8', 'Content 8', 'img8.jpg', 2);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 9', 'Content 9', 'img9.jpg', 2);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 10', 'Content 10', 'img10.jpg', 2);
+
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 11', 'Content 11', 'img11.jpg', 3);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 12', 'Content 12', 'img12.jpg', 3);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 13', 'Content 13', 'img13.jpg', 3);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 14', 'Content 14', 'img14.jpg', 3);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 15', 'Content 15', 'img15.jpg', 3);
+
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 16', 'Content 16', 'img16.jpg', 4);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 17', 'Content 17', 'img17.jpg', 4);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 18', 'Content 18', 'img18.jpg', 4);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 19', 'Content 19', 'img19.jpg', 4);
+INSERT INTO board (title, content, image, member_id) VALUES ('Title 20', 'Content 20', 'img20.jpg', 4);
