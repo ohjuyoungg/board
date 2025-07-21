07aad68004663c2591c62d851a76f227ae1862f5
ohjuyoungg <ejrm12@naver.com>
2025-07-21 18:42:57 +0900

    feat: BoardController 삭제 구현

--- src/main/java/Board/Juyoung/controller/BoardController.java
+++ src/main/java/Board/Juyoung/controller/BoardController.java
@@ -6,6 +6,7 @@ import Board.Juyoung.service.BoardService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,14 @@ public class BoardController {
         boardService.write(memberId, boardWriteRequest);
         return ResponseEntity
             .ok()
+            .build();
+    }
+
+    @DeleteMapping("{memberId}")
+    public ResponseEntity<Void> delete(@PathVariable("memberId") Long memberId) {
+        boardService.delete(memberId);
+        return ResponseEntity
+            .noContent()
             .build();
     }
 
