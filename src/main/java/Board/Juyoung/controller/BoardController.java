cca8fd9d07b4cefc62bba532a644fb4109e55d31
ohjuyoungg <ejrm12@naver.com>
2025-07-21 19:53:46 +0900

    feat: update, get Controller 구현

--- src/main/java/Board/Juyoung/controller/BoardController.java
+++ src/main/java/Board/Juyoung/controller/BoardController.java
@@ -1,5 +1,6 @@
 package Board.Juyoung.controller;
 
+import Board.Juyoung.controller.dto.request.BoardUpdateRequest;
 import Board.Juyoung.controller.dto.request.BoardWriteRequest;
 import Board.Juyoung.controller.dto.response.BoardResponse;
 import Board.Juyoung.service.BoardService;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +38,20 @@ public class BoardController {
         return ResponseEntity
             .noContent()
             .build();
+    }
+
+    @PutMapping("/{memberId}")
+    public ResponseEntity<Void> update(@PathVariable("memberId") Long memberId,
+        @RequestBody BoardUpdateRequest boardUpdateRequest) {
+        boardService.update(boardUpdateRequest, memberId);
+        return ResponseEntity
+            .noContent()
+            .build();
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<BoardResponse> getBoard(@PathVariable("memberId") Long memberId) {
+        return ResponseEntity.ok(boardService.getBoard(memberId));
     }
 
     @GetMapping
