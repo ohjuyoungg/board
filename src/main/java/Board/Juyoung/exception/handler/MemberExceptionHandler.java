b8d6308439a1e04f6e52faa877d740418e1012c1
ohjuyoungg <ejrm12@naver.com>
2025-08-08 23:34:44 +0900

    fix: GlobalExceptionHandler 클래스로만 잡히는 버그 테스트

--- src/main/java/Board/Juyoung/exception/handler/MemberExceptionHandler.java
+++ src/main/java/Board/Juyoung/exception/handler/MemberExceptionHandler.java
@@ -1,17 +1,17 @@
 package Board.Juyoung.exception.handler;
 
-import Board.Juyoung.controller.BoardController;
-import Board.Juyoung.controller.MemberController;
 import Board.Juyoung.exception.custom.MemberNotFoundException;
 import Board.Juyoung.exception.dto.ErrorResponse;
 import java.time.LocalDateTime;
 import java.util.Collections;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(assignableTypes = {MemberController.class, BoardController.class})
+@RestControllerAdvice
+@Slf4j
 public class MemberExceptionHandler {
 
     /**
@@ -19,6 +19,7 @@ public class MemberExceptionHandler {
      */
     @ExceptionHandler(MemberNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleMemberNotFound(MemberNotFoundException ex) {
+        log.error("test");
         ErrorResponse response = ErrorResponse.builder()
             .code("MEMBER_NOT_FOUND")
             .message(ex.getMessage()) // 예외에서 전달된 메시지 사용
