6ea07d35964ff06a0a73bea3994a6dd2918e235d
ohjuyoungg <ejrm12@naver.com>
2025-08-08 21:53:36 +0900

    feat: 게시판을 찾을 수 없을 때 발생하는 예외 클래스 BoardNotFoundException 클래스 구현

---
+++ src/main/java/Board/Juyoung/exception/custom/BoardNotFoundException.java
@@ -0,0 +1,8 @@
+package Board.Juyoung.exception.custom;
+
+public class BoardNotFoundException extends RuntimeException {
+
+    public BoardNotFoundException(String message) {
+        super(message);
+    }
+}
