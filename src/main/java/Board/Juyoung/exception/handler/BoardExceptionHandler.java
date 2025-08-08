4bb7196e9f873927551dbebd5056ec5db1643f89
ohjuyoungg <ejrm12@naver.com>
2025-08-09 00:36:28 +0900

    fix: GlobalExceptionHandler BoardExceptionHandler @Order(0) 버그 수정

--- src/main/java/Board/Juyoung/exception/handler/BoardExceptionHandler.java
+++ src/main/java/Board/Juyoung/exception/handler/BoardExceptionHandler.java
@@ -5,12 +5,14 @@ import Board.Juyoung.exception.custom.BoardPermissionDeniedException;
 import Board.Juyoung.exception.dto.ErrorResponse;
 import java.time.LocalDateTime;
 import java.util.Collections;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Order(0)
 public class BoardExceptionHandler {
 
     /**
