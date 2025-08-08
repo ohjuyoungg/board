f5e056778ea915fe53bda9b26568a265033d67a0
ohjuyoungg <ejrm12@naver.com>
2025-08-08 21:48:25 +0900

    refactor: 예외 처리 필드 리팩토링

--- src/main/java/Board/Juyoung/controller/MemberController.java
+++ src/main/java/Board/Juyoung/controller/MemberController.java
@@ -3,21 +3,17 @@ package Board.Juyoung.controller;
 import Board.Juyoung.controller.dto.request.MemberCreateRequest;
 import Board.Juyoung.controller.dto.request.MemberUpdateRequest;
 import Board.Juyoung.controller.dto.response.MemberResponse;
-import Board.Juyoung.exception.dto.ErrorResult;
 import Board.Juyoung.service.MemberService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -26,12 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExceptionHandler(IllegalArgumentException e) {
-        return new ErrorResult("잘못된 입력값입니다.", e.getMessage());
-    }
 
     @PostMapping
     public ResponseEntity<Void> register(@RequestBody MemberCreateRequest memberCreateRequest) {
