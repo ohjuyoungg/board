5892e4931f818165b7d4271bbfbfbbd0bf8c87ef
ohjuyoungg <ejrm12@naver.com>
2025-08-13 22:29:42 +0900

    feat: UserLoginResponse DTO 구현

--- src/main/java/Board/Juyoung/service/dto/response/UserLoginResponse.java
+++ src/main/java/Board/Juyoung/service/dto/response/UserLoginResponse.java
@@ -1,2 +1,13 @@
-package Board.Juyoung.service.dto.response;public class UserLoginResponse {
+package Board.Juyoung.service.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginResponse {
+
+    private String role;
+    private String name;
+    private String username;
 }
