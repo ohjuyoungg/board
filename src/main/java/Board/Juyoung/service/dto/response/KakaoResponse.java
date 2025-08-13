8de48fe3efccdcf764c53ed91a274a50ab28307c
ohjuyoungg <ejrm12@naver.com>
2025-08-14 04:14:35 +0900

    feat: OAuth KakaoResponse 클래스 구현

--- src/main/java/Board/Juyoung/service/dto/response/KakaoResponse.java
+++ src/main/java/Board/Juyoung/service/dto/response/KakaoResponse.java
@@ -1,2 +1,41 @@
-package Board.Juyoung.service.dto.response;public class KakaoResponse {
+package Board.Juyoung.service.dto.response;
+
+import java.util.Map;
+
+public class KakaoResponse implements OAuth2Response {
+
+    private final Map<String, Object> attributes;
+    private final Map<String, Object> kakaoAccount;
+    private final Map<String, String> profile;
+
+    public KakaoResponse(Map<String, Object> attributes) {
+        this.attributes = attributes;
+        this.kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        this.profile = (Map<String, String>) kakaoAccount.get("profile");
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getName() {
+        return profile.get("nickname");
+    }
+
+    @Override
+    public String getProfileImage() {
+        return profile.get("profile_image_url");
+    }
+
+    @Override
+    public String getEmail() {
+        return kakaoAccount.get("email").toString();
+    }
 }
