d745653169198255740fc09ee92bcb7febce2adc
ohjuyoungg <ejrm12@naver.com>
2025-08-13 22:29:16 +0900

    feat: OAuth2 GoogleResponse 클래스 구현

--- src/main/java/Board/Juyoung/service/dto/response/GoogleResponse.java
+++ src/main/java/Board/Juyoung/service/dto/response/GoogleResponse.java
@@ -1,2 +1,32 @@
-package Board.Juyoung.service.dto.response;public class GoogleResponse {
+package Board.Juyoung.service.dto.response;
+
+import java.util.Map;
+
+public class GoogleResponse implements OAuth2Response {
+
+    private final Map<String, Object> attribute;
+
+    public GoogleResponse(Map<String, Object> attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("sub").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attribute.get("name").toString();
+    }
 }
