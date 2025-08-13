7130eabdc066fe3b93e5faf747b583cc65c1fc1d
ohjuyoungg <ejrm12@naver.com>
2025-08-14 04:13:04 +0900

    feat: profileImage 매개변수 추가

--- src/main/java/Board/Juyoung/service/dto/request/MemberCreateRequest.java
+++ src/main/java/Board/Juyoung/service/dto/request/MemberCreateRequest.java
@@ -2,6 +2,7 @@ package Board.Juyoung.service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record MemberCreateRequest(@NotBlank String email, @NotBlank String password, String nickname) {
+public record MemberCreateRequest(String loginId, @NotBlank String email, @NotBlank String password, String nickname,
+                                  String profileImage) {
 
 }
