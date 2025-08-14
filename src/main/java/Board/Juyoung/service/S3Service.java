627c501d5c4826a96db511a2ffd4ed2f3afe8eb2
ohjuyoungg <ejrm12@naver.com>
2025-08-14 23:35:31 +0900

    refactor: 사용하지 않는 resion 필드 삭제

--- src/main/java/Board/Juyoung/service/S3Service.java
+++ src/main/java/Board/Juyoung/service/S3Service.java
@@ -23,9 +23,6 @@ public class S3Service {
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
-    @Value("${spring.cloud.aws.region.static}")
-    private String region;
-
 
     public String uploadImage(MultipartFile image) {
         String originalFilename = image.getOriginalFilename();
