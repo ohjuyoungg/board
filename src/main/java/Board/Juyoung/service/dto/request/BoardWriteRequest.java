1972f007d39f7f1255e42d43672568a0a05ab70a
ohjuyoungg <ejrm12@naver.com>
2025-08-14 19:53:00 +0900

    refactor: Multipart image record 클래스 내에서 삭제

--- src/main/java/Board/Juyoung/service/dto/request/BoardWriteRequest.java
+++ src/main/java/Board/Juyoung/service/dto/request/BoardWriteRequest.java
@@ -2,6 +2,6 @@ package Board.Juyoung.service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record BoardWriteRequest(@NotBlank String title, @NotBlank String content, String image) {
+public record BoardWriteRequest(@NotBlank String title, @NotBlank String content) {
 
 }
