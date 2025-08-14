0999dc5950e649dd06e7d59fcba81d60912403ae
ohjuyoungg <ejrm12@naver.com>
2025-08-14 19:53:23 +0900

    feat: AWS 이미지 업로드를 위한 S3Service 클래스 구현

--- src/main/java/Board/Juyoung/service/S3Service.java
+++ src/main/java/Board/Juyoung/service/S3Service.java
@@ -1,2 +1,43 @@
-package Board.Juyoung.service;public class S3Service {
+package Board.Juyoung.service;
+
+import java.io.IOException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    public String uploadImage(MultipartFile image) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+            .bucket(bucket)
+            .key(fileName)
+            .contentType(image.getContentType())
+            .contentLength(image.getSize())
+            .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(image.getInputStream(), image.getSize()));
+
+        return getPublicUrl(fileName);
+    }
+
+    private String getPublicUrl(String fileName) {
+        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucket, region, fileName);
+    }
 }
