0b624f826dc043f3a1fa4e38a6b7831a792f4fbc
ohjuyoungg <ejrm12@naver.com>
2025-08-14 22:08:34 +0900

    fix: s3Client -> s3Template 필드 수정

--- src/main/java/Board/Juyoung/service/S3Service.java
+++ src/main/java/Board/Juyoung/service/S3Service.java
@@ -1,43 +1,71 @@
 package Board.Juyoung.service;
 
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Template;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 @RequiredArgsConstructor
 public class S3Service {
 
-    private final S3Client s3Client;
+    private final S3Template s3Template;
 
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${spring.cloud.aws.region.static}")
     private String region;
 
-    public String uploadImage(MultipartFile image) throws IOException {
-        String fileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
 
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-            .bucket(bucket)
-            .key(fileName)
-            .contentType(image.getContentType())
-            .contentLength(image.getSize())
-            .build();
+    public String uploadImage(MultipartFile image) {
+        String originalFilename = image.getOriginalFilename();
+        if (originalFilename == null || originalFilename.isBlank()) {
+            throw new IllegalArgumentException("File name is missing.");
+        }
 
-        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(image.getInputStream(), image.getSize()));
+        String extension = "";
+        int lastDotIndex = originalFilename.lastIndexOf('.');
+        if (lastDotIndex != -1) {
+            extension = originalFilename.substring(lastDotIndex);
+        }
+        String key = UUID.randomUUID() + extension;
+        URL url;
 
-        return getPublicUrl(fileName);
+        try (InputStream imageInputStream = image.getInputStream()) {
+            url = s3Template.upload(
+                bucket,
+                key,
+                imageInputStream,
+                ObjectMetadata.builder()
+                    .contentType(image.getContentType())
+                    .build()
+            ).getURL();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return url.toString();
     }
 
-    private String getPublicUrl(String fileName) {
-        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucket, region, fileName);
+    public void deleteImage(String url) {
+        String key;
+        try {
+            key = new URI(url)
+                .toURL()
+                .getPath()
+                .substring(1);
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new RuntimeException("S3 Error", e);
+        }
+        s3Template.deleteObject(bucket, key);
     }
 }
