bc8ba064e4788caf913360275593853bde819aae
ohjuyoungg <ejrm12@naver.com>
2025-08-15 23:24:36 +0900

    feat: CommentService 클래스 구현 및 댓글 쓰기 기능 메서드 구현

--- src/main/java/Board/Juyoung/service/CommentService.java
+++ src/main/java/Board/Juyoung/service/CommentService.java
@@ -1,2 +1,33 @@
-package Board.Juyoung.service;public class CommentService {
+package Board.Juyoung.service;
+
+import Board.Juyoung.entity.Board;
+import Board.Juyoung.entity.Comment;
+import Board.Juyoung.entity.Member;
+import Board.Juyoung.exception.custom.BoardNotFoundException;
+import Board.Juyoung.exception.custom.MemberNotFoundException;
+import Board.Juyoung.repository.BoardRepository;
+import Board.Juyoung.repository.CommentRepository;
+import Board.Juyoung.repository.MemberRepository;
+import Board.Juyoung.service.dto.request.CommentWriteRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public void write(Long memberId, Long boardId, CommentWriteRequest commentWriteRequest) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new MemberNotFoundException("존재하지 않는 유저입니다."));
+        Board board = boardRepository.findById(boardId)
+            .orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시판입니다."));
+        Comment comment = new Comment(commentWriteRequest.content(), board, member);
+        commentRepository.save(comment);
+    }
 }
