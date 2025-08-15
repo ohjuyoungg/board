961c4d14781261d709ad205d378c907568f4de6c
ohjuyoungg <ejrm12@naver.com>
2025-08-16 00:00:11 +0900

    refactor: 웹 환경으로 Slice 무한 스크롤 페이징 -> Page 로 변경

--- src/main/java/Board/Juyoung/repository/CommentRepository.java
+++ src/main/java/Board/Juyoung/repository/CommentRepository.java
@@ -1,11 +1,11 @@
 package Board.Juyoung.repository;
 
 import Board.Juyoung.entity.Comment;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    Slice<Comment> findByBoardIdOrderByCreatedDateAsc(Long boardId, Pageable pageable);
+    Page<Comment> findByBoardIdOrderByCreatedDateAsc(Long boardId, Pageable pageable);
 }
