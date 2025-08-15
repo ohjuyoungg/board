1d946e0175b985a2194d7fc799e287a469112773
ohjuyoungg <ejrm12@naver.com>
2025-08-16 01:32:05 +0900

    feat: 게시물 단건 조회용 댓글 추가하여 record 수정

--- src/main/java/Board/Juyoung/service/dto/response/BoardResponse.java
+++ src/main/java/Board/Juyoung/service/dto/response/BoardResponse.java
@@ -2,12 +2,15 @@ package Board.Juyoung.service.dto.response;
 
 import Board.Juyoung.entity.Board;
 import Board.Juyoung.entity.Member;
+import java.util.List;
 
-public record BoardResponse(Long memberId, String nickname, Long boardId, String title, String content, String image) {
+public record BoardResponse(Long memberId, String nickname, Long boardId, String title, String content, String image,
+                            List<CommentResponse> comments) {
 
-    public static BoardResponse of(Board board) {
+    public static BoardResponse of(Board board, List<CommentResponse> comments) {
         Member member = board.getMember();
         return new BoardResponse(member.getId(), member.getNickname(), board.getId(), board.getTitle(),
-            board.getContent(), board.getImage());
+            board.getContent(), board.getImage(), comments
+        );
     }
 }
