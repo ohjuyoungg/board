bda0abfa644bbef49c299e1e06fd776e18f8ddd0
ohjuyoungg <ejrm12@naver.com>
2025-08-16 04:37:41 +0900

    fix: Cookie 삭제

--- src/main/java/Board/Juyoung/jwt/JWTFilter.java
+++ src/main/java/Board/Juyoung/jwt/JWTFilter.java
@@ -4,7 +4,6 @@ import Board.Juyoung.service.dto.CustomOAuth2User;
 import Board.Juyoung.service.dto.response.UserLoginResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -26,39 +25,28 @@ public class JWTFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
-
-        //cookie들을 불러온 뒤 Authorization Key에 담긴 쿠키를 찾음
+        // cookie들을 불러온 뒤 Authorization Key에 담긴 쿠키를 찾음
         String authorization = null;
-        Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
 
-            System.out.println(cookie.getName());
-            if (cookie.getName().equals("Authorization")) {
+        // 헤더를 통해서 찾음
+        authorization = request.getHeader("Authorization");
 
-                authorization = cookie.getValue();
-            }
-        }
-
-        //Authorization 헤더 검증
+        // Authorization 헤더 검증
         if (authorization == null) {
-
-            System.out.println("token null");
             filterChain.doFilter(request, response);
 
             //조건이 해당되면 메소드 종료 (필수)
             return;
         }
 
-        //토큰
-        String token = authorization;
+        // 토큰
+        String token = authorization.split(" ")[1];
 
-        //토큰 소멸 시간 검증
+        // 토큰 소멸 시간 검증
         if (jwtUtil.isExpired(token)) {
-
-            System.out.println("token expired");
             filterChain.doFilter(request, response);
 
-            //조건이 해당되면 메소드 종료 (필수)
+            // 조건이 해당되면 메소드 종료 (필수)
             return;
         }
 
@@ -79,7 +67,6 @@ public class JWTFilter extends OncePerRequestFilter {
             customOAuth2User.getAuthorities());
         //세션에 사용자 등록
         SecurityContextHolder.getContext().setAuthentication(authToken);
-
         filterChain.doFilter(request, response);
     }
 }
