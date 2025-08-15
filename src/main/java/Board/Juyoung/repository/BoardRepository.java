81db77d5ee1f592872615e54bd4f6c322e71a6bb
ohjuyoungg <ejrm12@naver.com>
2025-08-15 18:08:14 +0900

    feat: 생성일 기준 정렬 + 페이징으로 findByOrderCreatedAtDesc 메서드 추가

--- src/main/java/Board/Juyoung/repository/BoardRepository.java
+++ src/main/java/Board/Juyoung/repository/BoardRepository.java
@@ -1,8 +1,11 @@
 package Board.Juyoung.repository;
 
 import Board.Juyoung.entity.Board;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    Slice<Board> findByOrderByCreatedAtDesc(Pageable pageable);
 }
