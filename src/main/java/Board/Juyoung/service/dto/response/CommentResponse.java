787b92f6fd97eb4996335de6a7126a5f48e01e90
ohjuyoungg <ejrm12@naver.com>
2025-08-15 23:26:49 +0900

    feat: 댓글 응답 DTO 클래스 구현

--- src/main/java/Board/Juyoung/service/dto/response/CommentResponse.java
+++ src/main/java/Board/Juyoung/service/dto/response/CommentResponse.java
@@ -1,2 +1,14 @@
-package Board.Juyoung.service.dto.response;public class CommentResponse {
+package Board.Juyoung.service.dto.response;
+
+
+public record CommentResponse(
+    Long commentId,
+    Long memberId,
+    String nickname,
+    String content
+) {
+
+    public static CommentResponse of(Long commentId, Long memberId, String nickname, String content) {
+        return new CommentResponse(commentId, memberId, nickname, content);
+    }
 }
