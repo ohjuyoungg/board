7f6eb271665f7bac169d33a5fcc089403163615f
ohjuyoungg <ejrm12@naver.com>
2025-08-16 01:30:40 +0900

    refactor: 댓글 조회 컨트롤러 주석 처리

--- src/main/java/Board/Juyoung/controller/CommentController.java
+++ src/main/java/Board/Juyoung/controller/CommentController.java
@@ -3,17 +3,11 @@ package Board.Juyoung.controller;
 import Board.Juyoung.service.CommentService;
 import Board.Juyoung.service.dto.request.CommentUpdateRequest;
 import Board.Juyoung.service.dto.request.CommentWriteRequest;
-import Board.Juyoung.service.dto.response.CommentResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -59,11 +53,11 @@ public class CommentController {
             .build();
     }
 
-    @Operation(summary = "댓글 조회")
+  /* @Operation(summary = "댓글 조회")
     @GetMapping("/{boardId}")
     public ResponseEntity<Page<CommentResponse>> getComments(@PathVariable("boardId") Long boardId,
         @PageableDefault(size = 3, sort = "createdDate", direction = Direction.ASC)
         Pageable pageable) {
         return ResponseEntity.ok(commentService.getComments(boardId, pageable));
-    }
+    } */
 }
