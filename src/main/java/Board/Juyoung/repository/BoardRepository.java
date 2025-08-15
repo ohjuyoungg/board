5cf8b2294a593f8eab380efb2f03a3eac2bbce09
ohjuyoungg <ejrm12@naver.com>
2025-08-15 23:59:51 +0900

    refactor: 웹 환경으로 Slice 무한 스크롤 페이징 -> Page 로 변경

--- src/main/java/Board/Juyoung/repository/BoardRepository.java
+++ src/main/java/Board/Juyoung/repository/BoardRepository.java
@@ -1,11 +1,11 @@
 package Board.Juyoung.repository;
 
 import Board.Juyoung.entity.Board;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    Slice<Board> findByOrderByCreatedDateDesc(Pageable pageable);
+    Page<Board> findByOrderByCreatedDateDesc(Pageable pageable);
 }
