ae948d81ff9d85288972db3f26685c4a93f4781d
ohjuyoungg <ejrm12@naver.com>
2025-08-16 00:18:35 +0900

    feat: 댓글 조회 컨트롤러 메서드 구현 및 페이징 오름차순 조회 구현

--- src/main/java/Board/Juyoung/controller/CommentController.java
+++ src/main/java/Board/Juyoung/controller/CommentController.java
@@ -1,14 +1,22 @@
 package Board.Juyoung.controller;
 
 import Board.Juyoung.service.CommentService;
+import Board.Juyoung.service.dto.request.CommentUpdateRequest;
 import Board.Juyoung.service.dto.request.CommentWriteRequest;
+import Board.Juyoung.service.dto.response.CommentResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +48,22 @@ public class CommentController {
             .build();
     }
 
+    @Operation(summary = "댓글 수정")
+    @PutMapping("/{memberId}/{commentId}")
+    public ResponseEntity<Void> update(@PathVariable("memberId") Long memberId,
+        @PathVariable("commentId") Long commentId, CommentUpdateRequest commentUpdateRequest) {
+        commentService.update(memberId, commentId, commentUpdateRequest);
+        return ResponseEntity
+            .noContent()
+            .build();
+    }
+
+    @Operation(summary = "댓글 조회")
+    @GetMapping("/{boardId}")
+    public ResponseEntity<Page<CommentResponse>> getComments(@PathVariable("boardId") Long boardId,
+        @PageableDefault(size = 3, sort = "createdDate", direction = Direction.ASC)
+        Pageable pageable) {
+        return ResponseEntity.ok(commentService.getComments(boardId, pageable));
+    }
 
 }
