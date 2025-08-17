256e96020f01d93bbcffa988dce0bb9b03569295
ohjuyoungg <ejrm12@naver.com>
2025-08-17 16:13:56 +0900

    feat: N + 1 문제 해결하기 위해 @EntityGraph 애노테이션 추가

--- src/main/java/Board/Juyoung/repository/CommentRepository.java
+++ src/main/java/Board/Juyoung/repository/CommentRepository.java
@@ -3,9 +3,11 @@ package Board.Juyoung.repository;
 import Board.Juyoung.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    @EntityGraph(attributePaths = {"member", "board"})
     Page<Comment> findByBoardIdOrderByCreatedDateAsc(Long boardId, Pageable pageable);
 }
