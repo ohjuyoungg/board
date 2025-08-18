e149e3309b0ae1ae27ff5c90289f34da98265219
ohjuyoungg <ejrm12@naver.com>
2025-08-18 17:58:31 +0900

    refactor: DTO 내에 변경된 Getter 메서드 적용 및 수정

--- src/main/java/Board/Juyoung/service/dto/response/BoardResponse.java
+++ src/main/java/Board/Juyoung/service/dto/response/BoardResponse.java
@@ -4,13 +4,13 @@ import Board.Juyoung.entity.Board;
 import Board.Juyoung.entity.Member;
 import java.util.List;
 
-public record BoardResponse(Long memberId, String nickname, Long boardId, String title, String content, String image,
+public record BoardResponse(Long memberId, String nickname, Long boardId, String title, String content, String imageUrl,
                             List<CommentResponse> comments) {
 
     public static BoardResponse of(Board board, List<CommentResponse> comments) {
         Member member = board.getMember();
         return new BoardResponse(member.getId(), member.getNickname(), board.getId(), board.getTitle(),
-            board.getContent(), board.getImage(), comments
+            board.getContent(), board.getImageUrl(), comments
         );
     }
 }
