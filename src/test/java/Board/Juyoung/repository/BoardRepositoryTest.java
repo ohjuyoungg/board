faaa9b0aa73353d02b14a3e0e4c21c77c3b2dbb5
ohjuyoungg <ejrm12@naver.com>
2025-08-18 20:41:41 +0900

    style: 띄어쓰기 수정

--- src/test/java/Board/Juyoung/repository/BoardRepositoryTest.java
+++ src/test/java/Board/Juyoung/repository/BoardRepositoryTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package Board.Juyoung.repository;
+
+import Board.Juyoung.entity.Board;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 class BoardRepositoryTest {
-  
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    void testLazyLoading() {
+        var boards = boardRepository.findByOrderByCreatedDateDesc(PageRequest.of(0, 1));
+        for (Board board : boards.getContent()) {
+            System.out.println("제목: " + board.getTitle() + ", 작성자: " + board.getMember().getNickname());
+        }
+    }
 }
