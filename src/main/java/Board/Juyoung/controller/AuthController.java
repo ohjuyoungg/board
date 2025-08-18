d779abe89d144123f07d415bcfb7a3e1932b9ace
ohjuyoungg <ejrm12@naver.com>
2025-08-18 17:11:37 +0900

    feat: AuthController 내에 Swagger 설정 @Tag, @Operation 기능 구현

--- src/main/java/Board/Juyoung/controller/AuthController.java
+++ src/main/java/Board/Juyoung/controller/AuthController.java
@@ -1,6 +1,8 @@
 package Board.Juyoung.controller;
 
 import Board.Juyoung.jwt.JWTUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "access Token 발급")
 @RequiredArgsConstructor
 @RequestMapping("/api/auths")
 @RestController
@@ -16,6 +19,7 @@ public class AuthController {
 
     private final JWTUtil jwtUtil;
 
+    @Operation(summary = "새로운 access Token 발급")
     @PostMapping("/reissue-accessToken")
     public ResponseEntity<Void> reissueAccessToken(
         @CookieValue(value = "refreshToken") String refreshToken,
