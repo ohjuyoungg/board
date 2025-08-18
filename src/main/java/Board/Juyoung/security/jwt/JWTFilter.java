81a626a27c2c6475251f6d7aa4f075cc157862c3
ohjuyoungg <ejrm12@naver.com>
2025-08-18 18:40:08 +0900

    refactor: @RequiredArgsConstructor 애노테이션 적용

--- src/main/java/Board/Juyoung/security/jwt/JWTFilter.java
+++ src/main/java/Board/Juyoung/security/jwt/JWTFilter.java
@@ -7,20 +7,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@RequiredArgsConstructor
 @Component
 public class JWTFilter extends OncePerRequestFilter {
 
     private final JWTUtil jwtUtil;
-
-    public JWTFilter(JWTUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
