a8a3c14ba8cd66253d264239af79a1ab5378b3e5
ohjuyoungg <ejrm12@naver.com>
2025-08-18 21:14:02 +0900

    refactor: @RequiredArgsConstructor 애노테이션 적용

--- src/main/java/Board/Juyoung/security/user/CustomOAuth2User.java
+++ src/main/java/Board/Juyoung/security/user/CustomOAuth2User.java
@@ -4,16 +4,14 @@ import Board.Juyoung.service.dto.response.UserLoginResponse;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+@RequiredArgsConstructor
 public class CustomOAuth2User implements OAuth2User {
 
     private final UserLoginResponse userDTO;
-
-    public CustomOAuth2User(UserLoginResponse userDTO) {
-        this.userDTO = userDTO;
-    }
 
     @Override
     public Map<String, Object> getAttributes() {
